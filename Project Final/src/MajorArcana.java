28b73ee24dafe05214f4f775b4846e1ce98fe15d
cfujimot <maddierook@Maddies-MacBook-Air.local>
2014-11-27 06:25:17 -0500

    attempted to populate MajorArcana set with the load method

--- Project Final/src/MajorArcana.java
+++ Project Final/src/MajorArcana.java
@@ -13,23 +13,34 @@ public class MajorArcana {
 	public String name;
 	public String mtgName;
 	public String uprightFlavor;
-	public String uprightOracle;
+	public String uprightMeaning;
+	public String reversedName;
 	public String reversedFlavor;
-	public String reversedOracle;
+	public String reversedMeaning;
 	
-	public MajorArcana(int number1, String name1, String mtgName1, String uprightFlavor1, String uprightOracle1, String reversedFlavor1, String reversedOracle1){
+	/**
+	 * 
+	 * @param number1
+	 * @param name1
+	 * @param mtgName1
+	 * @param uprightFlavor1
+	 * @param uprightMeaning1
+	 * @param reversedName1
+	 * @param reversedFlavor1
+	 * @param reversedMeaning1
+	 */
+	public MajorArcana(int number1, String name1, String mtgName1, String uprightFlavor1, String uprightMeaning1, String reversedName1, String reversedFlavor1, String reversedMeaning1){
 	
-		//TODO create an array with name, flavor text, and oracle text
+		//TODO create an array or set with name, flavor text, and oracle text
 		//TODO be able to load each of these MajorArcana objects as a value for the integer keys in a HashSet
 		this.number =  number1;
 		this.name = name1;
 		this.mtgName = mtgName1;
 		this.uprightFlavor = uprightFlavor1;
-		this.uprightOracle = uprightOracle1;
+		this.uprightMeaning = uprightMeaning1;
+		this.reversedName = reversedName1;
 		this.reversedFlavor = reversedFlavor1;
-		this.reversedOracle = reversedOracle1;
-		
-	
+		this.reversedMeaning = reversedMeaning1;
 	}
 	
 }
